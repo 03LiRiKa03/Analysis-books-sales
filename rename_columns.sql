1a78579def7527f06ef37f3c19db43c5fb4f2ab2
Olya_halosa <156482289+03LiRiKa03@users.noreply.github.com>
2025-02-24 10:51:46 +0200

    Update rename_columns.sql

--- rename_columns.sql
+++ rename_columns.sql
@@ -1,17 +1,1 @@
-SELECT 
-   "index" AS index_id,
-   "Publishing Year" AS publishing_year,
-   "Book Name" AS book_name, 
-   "Author" AS author,
-   "language_code" AS language_code,
-   "Author_Rating" AS author_rating,
-   "Book_average_rating" AS book_average_rating,
-   "Book_ratings_count" AS book_ratings_count,
-   "genre" AS genre,
-   "gross sales" AS gross_sales,
-   "publisher revenue" AS publisher_revenue,
-   "sale price" AS sale_price,
-   "sales rank" AS sales_rank,
-   "Publisher" AS publisher,
-   "units sold" AS units_sold
-FROM "book_sales_dataset";
+
